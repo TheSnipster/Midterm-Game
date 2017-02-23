8ea36d25b1688c17d747a9469b0455f269880592
Snippy Nguyen <chevy666camaro@gmail.com>
2017-02-23 03:44:38 -0500

    Added simple mouseDrag script
    Found an acceptable script to drag object using mouse. Hold left button to drag, release button to drop. All objects are transformed to a fixed distance (tweakable) from camera => easier to drop into one place.
    Other scripts are mostly for X/Z plane view, does not account for spatial issues and very hard to move objects up without changing camera angles.  
    Custom mouse cursor when hover, drag and normal.

---
+++ Assets/Scripts/PickUpRaycast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpRaycast : MonoBehaviour {
+
+	Collider currentlyHeld;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray (Camera.main.transform.position, Camera.main.transform.forward);
+
+		RaycastHit rayHit = new RaycastHit ();
+
+		Debug.DrawRay (ray.origin, ray.direction * 5f, Color.cyan);
+
+		if (Physics.Raycast (ray, out rayHit, 20f)) {
+			if (Input.GetMouseButtonDown (0)) {
+				currentlyHeld = rayHit.collider;
+				currentlyHeld.transform.parent = Camera.main.transform;
+			}
+		}
+
+		if (Input.GetMouseButtonDown (0) == false && currentlyHeld != null) {
+			currentlyHeld.transform.parent = null;
+			currentlyHeld = null; 
+		}
+
+	}
+}

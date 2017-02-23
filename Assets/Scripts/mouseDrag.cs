7ae60e50d90a9d480847255b416088cd53dc932c
Snippy Nguyen <chevy666camaro@gmail.com>
2017-02-23 04:55:37 -0500

    Added a Checklist for the Ingredients
    Scalable list depending on how many button is on the panel. Will figure out a way to turn it into a checklist later on.

--- Assets/Scripts/mouseDrag.cs
+++ Assets/Scripts/mouseDrag.cs
@@ -6,7 +6,7 @@ public class mouseDrag : MonoBehaviour {
 	float distance = 45;
 
 	void OnMouseDrag(){
-		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y + 5f, distance);
 
 		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
 

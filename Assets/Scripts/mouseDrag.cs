10378c17f518f6da9864a89a615186a19e5f530b
Snippy Nguyen <chevy666camaro@gmail.com>
2017-02-23 04:16:36 -0500

    Added mouseLook Script
    Move camera with the mouse direction. Fiddled with some numbers so the view doesn't go wonkers.

--- Assets/Scripts/mouseDrag.cs
+++ Assets/Scripts/mouseDrag.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class mouseDrag : MonoBehaviour {
-	float distance = 35;
+	float distance = 45;
 
 	void OnMouseDrag(){
 		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
